d8aed7a18f0b028c73487d562b8514851bbadfe8
keagan erasmus <keaganerasmus12@gmail.com>
2021-08-29 16:09:09 +1200

    added basic movement to the cube

--- display_wireframe.py
+++ display_wireframe.py
@@ -2,6 +2,15 @@ import sys
 import wireframe
 import pygame
 
+key_to_function = {
+    pygame.K_LEFT:   (lambda x: x.translateAll('x', -10)),
+    pygame.K_RIGHT:  (lambda x: x.translateAll('x',  10)),
+    pygame.K_DOWN:   (lambda x: x.translateAll('y',  10)),
+    pygame.K_UP:     (lambda x: x.translateAll('y', -10)),
+    pygame.K_EQUALS: (lambda x: x.scaleAll(1.25)),
+    pygame.K_MINUS:  (lambda x: x.scaleAll( 0.8))
+    }
+
 class ProjectionViewer:
     #display 3D object on a pygame instance
 
@@ -29,6 +38,9 @@ class ProjectionViewer:
                 if event.type == pygame.QUIT:
                     running = False
                     exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key in key_to_function:
+                        key_to_function[event.key](self)
             
             self.display()
             pygame.display.flip()
@@ -48,6 +60,18 @@ class ProjectionViewer:
             if self.displayNodes:
                 for node in wireframe.nodes:
                     pygame.draw.circle(self.screen, self.nodeColour, (int(node.x), int(node.y)), self.nodeRadius, 0)
+
+    def translateAll(self, axis, d):
+        for wireframe in self.wireframes.values():
+            wireframe.translate(axis, d)
+    
+    def scaleAll(self, scale):
+        center_x = self.width/2
+        center_y = self.height/2
+
+        for wireframe in self.wireframes.values():
+            wireframe.scale((center_x, center_y), scale)
+
 
 if __name__ == "__main__":
     pv = ProjectionViewer(400, 300)
--- wireframe.py
+++ wireframe.py
@@ -33,7 +33,18 @@ class Wireframe:
             print(f"{i} ({float(edge.start.x)}, {float(edge.start.y)}, {float(edge.start.z)})")
             print(f"to ({float(edge.stop.x)}, {float(edge.stop.y)}, {float(edge.stop.z)})")
 
-
+    def translate(self, axis, d):
+        if axis in ['x', 'y', 'z']:
+            for node in self.nodes:
+                setattr(node, axis, getattr(node, axis) + d)
+    
+    def scale(self, center, scale):
+        """ Scale the wireframe from the centre of the screen """
+        center_x, center_y = center
+        for node in self.nodes:
+            node.x = center_x + scale * (node.x - center_x)
+            node.y = center_y + scale * (node.y - center_y)
+            node.z *= scale
 # my_wireframe = Wireframe()
 # my_wireframe.addNodes([(0,0,0), (1,2,3), (3,2,1)])
 # my_wireframe.addNodes([(1,2)])
@@ -45,6 +56,6 @@ if __name__ == "__main__":
     cube.addEdges([(n,n+4) for n in range(0,4)])
     cube.addEdges([(n,n+1) for n in range(0, 8, 2)])
     cube.addEdges([(n,n+2) for n in (0,1,4,5)])
-    
+
     cube.outputNodes()
     cube.outputEdges()
